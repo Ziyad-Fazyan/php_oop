490f4970aeac66306af451aefe189de1c8331299
Ziyad-Fazyan <ziyadfazyan2@gmail.com>
2024-12-03 10:43:28 +0700

    bismillah

--- data/Person.php
+++ data/Person.php
@@ -1,7 +1,32 @@
 <?php
 
 class Person {
-    var $name;
-    var $age;
-    var $city;
+    var string $name;
+    var int $age;
+    var string $city;
+    var string $country; //= "Indonesia" default value
+
+    const AUTHOR = "Ziyad";
+
+    function __construct(
+        string $name = "",
+        int $age = 0,
+        string $city = "",
+        string $country = "Indonesia" //= "Indonesia" default value
+    ) {
+        $this->name = $name;
+        $this->age = $age;
+        $this->city = $city;
+        $this->country = $country;
+    }
+
+    function tahunLahir() {
+        return 2024 - $this->age;
+        //this adalah keyword untuk mengakses property dari class yang sama
+    }
+
+    function penulis() {
+        return self::AUTHOR . " Fazyan";
+        //self adalah keyword untuk mengakses property dari class yang sama
+    }
 }
--- tes.php
+++ tes.php
@@ -2,22 +2,36 @@
 
 require_once "data/Person.php";
 
-$person = new Person();
-$person->name = "John";
-$person->age = 30;
-$person->city = "New York";
+// $person = new Person();
+// $person->name = "Ziyad";
+// $person->age = 19;
+// $person->city = "New York";
+// echo "Tahub lahir $person->name : " . (2024 - $person->age) . "\n";
+// echo "Tahub lahir $person->name : " . $person->tahunLahir() . "\n";
+// var_dump($person);
+
+// $fulan = new Person();
+// $fulan->name = "Fulan";
+// $fulan->age = 25;
+// $fulan->city = "Jakarta";
+// echo "Tahub lahir $fulan->name : " . $fulan->tahunLahir() . "\n";
+// var_dump($fulan);
+
+// $fulanah = new Person();
+// $fulanah->name = "Fulanah";
+// $fulanah->age = 35;
+// $fulanah->city = "Bandung";
+// echo "Tahub lahir $fulanah->name : " . $fulanah->tahunLahir() . "\n";
+// var_dump($fulanah);
+
+// echo "Penulis script ini adalah : " . Person::AUTHOR . "\n";
+// echo "Penulis script ini adalah : " . $person->penulis() . "\n";
+
+$person = new Person("Fulan", 25, "Jakarta");
 var_dump($person);
-
-$fulan = new Person();
-$fulan->name = "Fulan";
-$fulan->age = 25;
-$fulan->city = "Jakarta";
+$fulan = new Person("Fulanah", 35, "Bandung");
 var_dump($fulan);
-
-$fulanah = new Person();
-$fulanah->name = "Fulanah";
-$fulanah->age = 35;
-$fulanah->city = "Bandung";
+$fulanah = new Person("Fulanah", 35, "Bandung");
 var_dump($fulanah);
 
 ?>
